c54251eb6d855a784ccf27a5fdc176b4d88678fc
fnwiya <fnwiya@gmail.com>
2015-05-19 22:25:34 +0900

    edit links

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,4 +4,5 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.conkyrc ~/.conkyrc
 ln -sf ~/dotfiles/.conky ~/.conky
 ln -sf ~/dotfiles/.vim ~/.vim
-
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
