7278b183393cd6874ca4c54f5d02cefa604ba203
fnwiya <fnwiya@gmail.com>
2015-05-20 12:17:59 +0900

    add byobu

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,6 +3,7 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.conkyrc ~/.conkyrc
 ln -sf ~/dotfiles/.conky ~/.conky
+ln -sf ~/dotfiles/.byobu ~/.byobu
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
