2d3c5ec81afc08625c718c638e4c70775f8a4dfb
fnwiya <fnwiya@gmail.com>
2015-10-31 18:04:43 +0900

    set .sbclrc

--- install/sbcl.sh
+++ install/sbcl.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+# https://www.quicklisp.org/beta/
+curl -O https://beta.quicklisp.org/quicklisp.lisp
+curl -O https://beta.quicklisp.org/quicklisp.lisp.asc
 sbcl --load quicklisp.lisp
+# * (quicklisp-quickstart:install)
+# * (ql:add-to-init-file)
+
+# * (ql:quickload :cl-ppcre) install
+# * (ql:system-apropos "web") search
+# * (ql:update-all-dists) update
