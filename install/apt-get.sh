79f0cc7f1a9cd1ef995923b1008d35ff0d0c834d
fnwiya <fnwiya@gmail.com>
2015-11-02 14:43:53 +0900

    set spt-get

--- install/apt-get.sh
+++ install/apt-get.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install dropbox
-sudo apt-get install shutter
+# develop-tool
 sudo apt-get install tmux
 sudo apt-get install zsh
 sudo apt-get install git
 sudo apt-get install wget
 sudo apt-get install build-essential
-sudo apt-get install python-setuptools
-sudo apt-get install python-pip
-sudo apt-get install python-numpy
-sudo apt-get install python-scipy
+# python
 sudo apt-get install python-matplotlib
+sudo apt-get install python-numpy
+sudo apt-get install python-pip
+sudo apt-get install python-setuptools
+sudo apt-get install python-scipy
+sudo apt-get install python-virtualenv
+# others
+sudo apt-get install dropbox
+sudo apt-get install shutter
