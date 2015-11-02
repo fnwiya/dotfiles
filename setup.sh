a2ae43afe4a616eeb94878c14bd7a3ae6029c393
fnwiya <fnwiya@gmail.com>
2015-11-02 12:06:31 +0900

    add shutter

--- setup.sh
+++ setup.sh
@@ -10,7 +10,8 @@ ln -sf ~/dotfiles/setup/bash/.bash_profile ~
 ln -sf ~/dotfiles/setup/.tmux.conf ~
 ln -sf ~/dotfiles/setup/.peco ~
 ln -sf ~/dotfiles/setup/.sbclrc ~
+ln -sf ~/dotfiles/setup/linux/.shutter ~
 #karabinerDir=~/Library/Application Support/Karabiner
 #if [ -e $karabinerDir ]; then
-#    ln -sf ~/dotfiles/install/osx/key4remap/private.xml $karabinerDir/private.xml
+#    ln -sf ~/dotfiles/setup/osx/key4remap/private.xml $karabinerDir/private.xml
 #fi
