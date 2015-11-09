4e2647426b9ec443fe59015261557b1624845881
fnwiya <fnwiya@gmail.com>
2015-11-09 16:11:03 +0900

    update apt-get

--- install/apt-get.sh
+++ install/apt-get.sh
@@ -3,10 +3,18 @@ sudo apt-get update
 sudo apt-get upgrade
 # develop-tool
 sudo apt-get install build-essential
+sudo apt-get install curl
+sudo apt-get install elixir
+sudo apt-get install fish
+sudo apt-get install gcc
 sudo apt-get install git
+sudo apt-get install global
+sudo apt-get install go
+sudo apt-get install leiningen
 sudo apt-get install sqlite3
 sudo apt-get install tmux
 sudo apt-get install wget
+sudo apt-get install z
 sudo apt-get install zsh
 # python
 sudo apt-get install python-matplotlib
