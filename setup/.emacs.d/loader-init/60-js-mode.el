b3b34265e5f66a5f506dc9d2e3674c92253254eb
fnwiya <fnwiya@gmail.com>
2015-11-11 08:56:00 +0900

    add

--- setup/.emacs.d/loader-init/60-js-mode.el
+++ setup/.emacs.d/loader-init/60-js-mode.el
@@ -1,8 +1,11 @@
 (use-package js2-mode
   :mode
   (("\.js$" . js2-mode)
-   ("\.json$" . js2-mode))
+   ("\.json$" . js2-mode)
+   ("\.jsx$" . js2-jsx-mode))
   :config
+  (flycheck-add-mode 'javascript-eslint 'js2-jsx-mode)
+  (add-hook 'js2-jsx-mode-hook 'flycheck-mode)
   )
 (use-package jquery-doc
   :commands (jquery-doc-setup)
