70a0d673732bc9d4a4c9571318c01d910176e02a
fnwiya <fnwiya@gmail.com>
2015-11-10 10:19:13 +0900

    edit setDotfiles

--- setDotfiles.sh
+++ setDotfiles.sh
@@ -4,18 +4,18 @@ DOTPATH=~/.dotfiles
 GITHUB_URL=https://github.com/fnwiya/dotfiles.git
 
 # git が使えるなら git
-if has "git"; then
+if [ -x "`which git`" ] ; then
     git clone "$GITHUB_URL" "$DOTPATH"
 
 # 使えない場合は curl か wget を使用する
-elif has "curl" || has "wget"; then
+elif [ -x "`which curl`" ] || [ -x "`which wget`" ] ; then
     tarball="https://github.com/fnwiya/dotfiles/archive/master.tar.gz"
 
     # どっちかでダウンロードして，tar に流す
-    if has "curl"; then
+    if [ -x "`which curl`" ] ; then
         curl -L "$tarball"
 
-    elif has "wget"; then
+    elif [ -x "`which wget`" ] ; then
         wget -O - "$tarball"
 
     fi | tar xv -
