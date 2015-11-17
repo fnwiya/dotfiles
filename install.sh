64ce8a88d6ad2cbd223c7e2c002d1896a5192798
fnwiya <fnwiya@gmail.com>
2015-11-17 12:31:50 +0900

    separate with distrybution

--- install.sh
+++ install.sh
@@ -33,12 +33,54 @@ case ${OSTYPE} in
         cp /usr/share/terminfo/65/eterm ~/.terminfo/65/eterm-color
         ;;
     linux*)
-        source install/apt-get.sh
-        source install/npm.sh
-        source install/pyenv.sh
-        source install/pip.sh
-        source install/sbcl.sh
-        source install/peco4linux.sh
+        if   [ -e /etc/debian_version ] ||
+                 [ -e /etc/debian_release ]; then
+            # Check Ubuntu or Debian
+            if [ -e /etc/lsb-release ]; then
+                # Ubuntu
+                distri_name="ubuntu"
+                source install/apt-get.sh
+                source install/npm.sh
+                source install/pyenv.sh
+                source install/pip.sh
+                source install/sbcl.sh
+                source install/peco4linux.sh
+            else
+                # Debian
+                distri_name="debian"
+            fi
+        elif [ -e /etc/fedora-release ]; then
+            # Fedra
+            distri_name="fedora"
+        elif [ -e /etc/redhat-release ]; then
+            if [ -e /etc/oracle-release ]; then
+                # Oracle Linux
+                distri_name="oracle"
+            else
+                # Red Hat Enterprise Linux
+                # CentOS
+                distri_name="redhat"
+            fi
+        elif [ -e /etc/turbolinux-release ]; then
+            # Turbolinux
+            distri_name="turbol"
+        elif [ -e /etc/SuSE-release ]; then
+            # SuSE Linux
+            distri_name="suse"
+        elif [ -e /etc/mandriva-release ]; then
+            # Mandriva Linux
+            distri_name="mandriva"
+        elif [ -e /etc/vine-release ]; then
+            # Vine Linux
+            distri_name="vine"
+        elif [ -e /etc/gentoo-release ]; then
+            # Gentoo Linux
+            distri_name="gentoo"
+        else
+            # Other
+            echo "unkown distribution"
+            distri_name="unkown"
+        fi
         ;;
 esac
 
