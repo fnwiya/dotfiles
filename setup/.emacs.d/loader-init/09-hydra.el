4888818ed00c2660e72b58c133d310ebd6219b1e
fnwiya <fnwiya@gmail.com>
2015-11-19 05:38:05 +0900

    update

--- setup/.emacs.d/loader-init/09-hydra.el
+++ setup/.emacs.d/loader-init/09-hydra.el
@@ -1,15 +1,15 @@
 (use-package hydra-examples
   :config
   (global-set-key
-   (kbd "C-M-s")
-   (defhydra hydra-splitter (:color red)
-     "splitter"
-     ("h" hydra-move-splitter-left)
-     ("j" hydra-move-splitter-down)
-     ("k" hydra-move-splitter-up)
-     ("l" hydra-move-splitter-right)))
-  (defhydra hydra-zoom (global-map "<f2>")
-  "zoom"
-  ("g" text-scale-increase "in")
-  ("l" text-scale-decrease "out"))
+    (kbd "C-M-s")
+      (defhydra hydra-splitter (:color red)
+        "splitter"
+        ("h" hydra-move-splitter-left)
+        ("j" hydra-move-splitter-down)
+        ("k" hydra-move-splitter-up)
+        ("l" hydra-move-splitter-right)))
+      (defhydra hydra-zoom (global-map "<f2>")
+        "zoom"
+        ("g" text-scale-increase "in")
+        ("l" text-scale-decrease "out"))
 )
