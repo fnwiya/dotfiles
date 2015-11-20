4e695d831d21fef67d2ef8542689b0cb88bb8307
fnwiya <fnwiya@gmail.com>
2015-11-20 09:03:43 +0900

    no-comp

--- setup/.emacs.d/init.el
+++ setup/.emacs.d/init.el
@@ -136,6 +136,7 @@
 ;  (add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
 ;  )
 
-(byte-recompile-directory (expand-file-name "~/.emacs.d/loader-init") 0)
+
+;(byte-recompile-directory (expand-file-name "~/.emacs.d/loader-init") 0)
 
 ;;; init.el ends here
