31ee0574cf2e4814a783f7d1874e2d24044056ae
fnwiya <fnwiya@gmail.com>
2015-11-30 03:30:40 +0900

    update

--- setup/.emacs.d/elisp/myElisp/meigen/meigen.el
+++ setup/.emacs.d/elisp/myElisp/meigen/meigen.el
@@ -7,12 +7,12 @@
 (require 'f)
 
 (defun random-call-message()
-  (interactive)
   (let* ((random-messages
           (split-string
            (f-read-text "~/.emacs.d/elisp/myElisp/meigen/meigen.txt") "\n"))
     (len (length random-messages)))
-    (car (nthcdr (random len) random-messages))))
+    (car (nthcdr (random len) random-messages)))
+  )
 
 (add-hook 'emacs-startup-hook
       (lambda ()
