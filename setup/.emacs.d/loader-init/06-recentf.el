ea0e1da4d588da0c4b75ce98b44de32b3927eea9
fnwiya <fnwiya@gmail.com>
2015-11-30 15:23:19 +0900

    update

--- setup/.emacs.d/loader-init/06-recentf.el
+++ setup/.emacs.d/loader-init/06-recentf.el
@@ -4,7 +4,7 @@
   :config
   (setq recentf-save-file "~/.emacs.d/cache/.recentf")
   (setq recentf-max-saved-items 1000)
-  (setq recentf-exclude '(".recentf"))
+  (setq recentf-exclude '(".recentf" "~/.emacs.d/cache/ido.last"))
   (setq recentf-auto-cleanup 30)
   (setq recentf-auto-save-timer
         (run-with-idle-timer 60 t 'recentf-save-list))
