554526d139f1171b6dfb4ad80eeb32bd2c761ee7
fnwiya <fnwiya@gmail.com>
2015-12-01 19:45:50 +0900

    edit file-name

--- setup/.emacs.d/loader-init/75-meigen.el
+++ setup/.emacs.d/loader-init/75-meigen.el
@@ -1,7 +1,7 @@
-(add-to-list 'load-path "~/.emacs.d/elisp/myElisp/meigen-bot")
+(add-to-list 'load-path "~/.emacs.d/elisp/work/meigen-bot")
 (use-package meigen-bot
   :config
-  (setq meigen-file-path "~/.emacs.d/elisp/myElisp/meigen-bot/meigen.txt")
+  (setq meigen-file-path "~/.emacs.d/elisp/work/meigen-bot/meigen.txt")
   (add-hook 'emacs-startup-hook
           (lambda ()
             (echo-meigen-to-minibuffer)
