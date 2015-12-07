d870788135c1dafe0c20b9612b617f96f3c1c931
fnwiya <fnwiya@gmail.com>
2015-12-07 23:24:12 +0900

    update

--- setup/.emacs.d/loader-init/88-make-directory-unless-directory-exists.el
+++ setup/.emacs.d/loader-init/88-make-directory-unless-directory-exists.el
@@ -1,10 +1,10 @@
 ;;; find-fileでディレクトリが無ければ作る
-(defun make-directory-unless-directory-exists ()
+(defun make-directory-unless-directory-exists()
   (let (
          (d (file-name-directory buffer-file-name))
        )
     (unless (file-directory-p d)
-      (when (y-or-n-p "No such directory: make directory?")
+      (when (yes-or-no-p "No such directory: make directory?")
         (make-directory d t))
       )
     )
