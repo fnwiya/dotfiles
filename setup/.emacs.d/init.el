a677ff2eb478f01b1ea51ba32f653a38e1bea670
fnwiya <fnwiya@gmail.com>
2015-12-12 17:16:14 +0900

    update

--- setup/.emacs.d/init.el
+++ setup/.emacs.d/init.el
@@ -13,6 +13,9 @@
   (byte-recompile-directory (expand-file-name "~/.emacs.d/themes") 0)
   (byte-compile-file "~/.emacs.d/init.el")
   )
+(save-window-excursion
+  (compile-inits)
+)
 (add-hook 'after-save-hook
           (lambda ()
             (if (eq major-mode 'emacs-lisp-mode)
