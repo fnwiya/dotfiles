476cfd51ff6d366762dc6bfd9c2b21e06056b6ef
fnwiya <fnwiya@gmail.com>
2015-12-13 18:20:39 +0900

    update

--- setup/.emacs.d/loader-init/21-color-theme.el
+++ setup/.emacs.d/loader-init/21-color-theme.el
@@ -1,8 +1,5 @@
 (when window-system
-  ;; (setq custom-theme-directory "~/.emacs.d/themes/")
-  ;; (load-theme 'fnwiya t)
-  ;; (enable-theme 'fnwiya)
-  (setq custom-theme-directory "~/.emacs.d/themes/emacs-material-theme")
-  (load-theme 'material t)
-  (enable-theme 'material)
+  (setq custom-theme-directory "~/.emacs.d/themes/")
+  (load-theme 'fnwiya t)
+  (enable-theme 'fnwiya)
 )
