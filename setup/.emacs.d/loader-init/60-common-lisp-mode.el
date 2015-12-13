fa8853189ed5e932e93e768dddba22421331070e
fnwiya <fnwiya@gmail.com>
2015-12-13 15:24:30 +0900

    update

--- setup/.emacs.d/loader-init/60-common-lisp-mode.el
+++ setup/.emacs.d/loader-init/60-common-lisp-mode.el
@@ -9,4 +9,17 @@
 
 (add-hook 'slime-repl-mode-hook
    '(lambda ()
-      (define-key slime-repl-mode-map "\C-c\M-r" 'slime-restart-inferior-lisp)))
+      (define-key slime-repl-mode-map "\C-c\M-r" 'slime-restart-inferior-lisp)
+      (define-key slime-repl-mode-map "C-8" 'backward-list)
+      (define-key slime-repl-mode-map "C-9" 'forward-list)
+      ))
+(add-hook 'lisp-mode-hook
+   '(lambda ()
+      (define-key lisp-mode-map "C-8" 'backward-list)
+      (define-key lisp-mode-map "C-9" 'forward-list)
+      ))
+(add-hook 'lisp-interaction-mode-hook
+   '(lambda ()
+      (define-key lisp-interaction-mode-map "C-8" 'backward-list)
+      (define-key lisp-interaction-mode-map "C-9" 'forward-list)
+      ))
--- setup/.emacs.d/loader-init/60-elisp-mode.el
+++ setup/.emacs.d/loader-init/60-elisp-mode.el
@@ -1,2 +1,7 @@
 (add-to-list 'auto-mode-alist '("\.el$" . emacs-lisp-mode))
 (add-to-list 'auto-mode-alist '("\.elc$" . emacs-lisp-mode))
+(add-hook 'emacs-lisp-mode-hook
+   '(lambda ()
+      (define-key emacs-lisp-mode-map "C-8" 'backward-list)
+      (define-key emacs-lisp-mode-map "C-9" 'forward-list)
+      ))
