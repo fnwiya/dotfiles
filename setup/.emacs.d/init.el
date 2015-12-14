1636e6dace6a432709c0884571e8b3ca9c4135bb
fnwiya <fnwiya@gmail.com>
2015-12-14 22:27:02 +0900

    update

--- setup/.emacs.d/init.el
+++ setup/.emacs.d/init.el
@@ -13,7 +13,8 @@
 
 (require 'package)
 (setq package-user-dir "~/.emacs.d/elisp/package/")
-;; (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
 (package-initialize)
 
 (eval-when-compile (require 'cl))
