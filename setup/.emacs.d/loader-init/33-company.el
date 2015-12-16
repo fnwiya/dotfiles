909b1d05e4d6cac07517d2a915eb09f79fa05990
fnwiya <fnwiya@gmail.com>
2015-12-16 09:08:28 +0900

    update

--- setup/.emacs.d/loader-init/33-company.el
+++ setup/.emacs.d/loader-init/33-company.el
@@ -1,12 +1,11 @@
 (use-package company
   :diminish company-mode
   :config
-  (global-company-mode +1)
+  (global-company-mode) ; 全バッファで有効にする
+  (setq company-idle-delay 0) ; デフォルトは0.5.自動補完を offにしたい場合はnilに設定する
+  (setq company-minimum-prefix-length 2) ; デフォルトは4
+  (setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
 
-  ;; 自動補完を offにしたい場合は, company-idle-delayを nilに設定する
-  ;; auto-completeでいうところの ac-auto-start にあたる.
-  (custom-set-variables
-   '(company-idle-delay nil))
   (set-face-attribute 'company-tooltip nil
                       :foreground "black" :background "lightgrey")
   (set-face-attribute 'company-tooltip-common nil
@@ -24,10 +23,13 @@
   (global-set-key (kbd "C-M-i") 'company-complete)
 
   ;; C-n, C-pで補完候補を次/前の候補を選択
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous)
   (define-key company-search-map (kbd "C-n") 'company-select-next)
   (define-key company-search-map (kbd "C-p") 'company-select-previous)
+  (define-key company-active-map (kbd "C-h") nil)
 
   ;; C-sで絞り込む
   (define-key company-active-map (kbd "C-s") 'company-filter-candidates)
@@ -37,4 +39,28 @@
 
   ;; 各種メジャーモードでも C-M-iで company-modeの補完を使う
   (define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete)
+
+  ;; 候補が１つの場合はそれを選択．複数の場合，挿入可能なprefixがあれば挿入し，なければcompany-select-next
+  ;; http://qiita.com/sune2/items/b73037f9e85962f5afb7
+  (defun company--insert-candidate2 (candidate)
+  (when (> (length candidate) 0)
+    (setq candidate (substring-no-properties candidate))
+    (if (eq (company-call-backend 'ignore-case) 'keep-prefix)
+        (insert (company-strip-prefix candidate))
+      (if (equal company-prefix candidate)
+          (company-select-next)
+          (delete-region (- (point) (length company-prefix)) (point))
+        (insert candidate))
+      )))
+
+(defun company-complete-common2 ()
+  (interactive)
+  (when (company-manual-begin)
+    (if (and (not (cdr company-candidates))
+             (equal company-common (car company-candidates)))
+        (company-complete-selection)
+      (company--insert-candidate2 company-common))))
+
+(define-key company-active-map [tab] 'company-complete-common2)
+(define-key company-active-map [backtab] 'company-select-previous) ; おまけ
   )
