6279c9e27a385a31d48ba28e93b0a845d59c1086
fnwiya <fnwiya@gmail.com>
2015-12-26 18:22:40 +0900

    update

--- setup/.emacs.d/loader-init/08-font.el
+++ setup/.emacs.d/loader-init/08-font.el
@@ -3,7 +3,7 @@
   (create-fontset-from-ascii-font "Ricty-14:weight=normal:slant=normal" nil "ricty")
   (set-fontset-font "fontset-ricty"
                     'unicode
-                    (font-spec :family "Ricty" :size 14)
+                    (font-spec :family "Ricty" :size 30)
                     nil
                     'append)
   (add-to-list 'default-frame-alist '(font . "fontset-ricty"))
