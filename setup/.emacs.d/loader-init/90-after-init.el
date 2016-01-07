5a641909320c99a25ff9660bdd3d9a85a743402d
fnwiya <fnwiya@gmail.com>
2016-01-07 20:57:08 +0900

    update

--- setup/.emacs.d/loader-init/90-after-init.el
+++ setup/.emacs.d/loader-init/90-after-init.el
@@ -18,8 +18,6 @@
 (when (window-system)
   (add-hook 'after-init-hook
             (lambda()
-              (save-window-excursion
-                (list-packages))
               (setq w (selected-window))
               (setq w2 (split-window w (- (window-height w) 8)))
               (select-window w2)
