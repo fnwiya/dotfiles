fd3c3f5c6f60cc63782c61a9978c1fff9062a540
fnwiya <fnwiya@gmail.com>
2016-01-11 20:16:19 +0900

    update

--- setup/.emacs.d/loader-init/67-pomodoro.el
+++ setup/.emacs.d/loader-init/67-pomodoro.el
@@ -1,20 +1,21 @@
-;;; http://syohex.hatenablog.com/entry/20121215/1355579575
-(add-to-list 'load-path "~/.emacs.d/elisp/vendor/pomodoro.el")
+(use-package org-pomodoro)
+;; ;;; http://syohex.hatenablog.com/entry/20121215/1355579575
+;; (add-to-list 'load-path "~/.emacs.d/elisp/vendor/pomodoro.el")
 
-;; 作業時間終了後に開くファイル。デフォルトでは "~/.emacs.d/pomodoro.org"
-(setq pomodoro:file "~/.emacs.d/mywork.txt")
+;; ;; 作業時間終了後に開くファイル。デフォルトでは "~/.emacs.d/pomodoro.org"
+;; (setq pomodoro:file "~/.emacs.d/mywork.txt")
 
-;; 作業終了後の hook
-(add-hook 'pomodoro:finish-work-hook
-          (lambda ()
-            (my/pomodoro-notification :body "Work is Finish")))
+;; ;; 作業終了後の hook
+;; (add-hook 'pomodoro:finish-work-hook
+;;           (lambda ()
+;;             (my/pomodoro-notification :body "Work is Finish")))
 
-;; 休憩終了後の hook
-(add-hook 'pomodoro:finish-rest-hook
-          (lambda ()
-            (my/pomodoro-notification :body "Break time is finished")))
+;; ;; 休憩終了後の hook
+;; (add-hook 'pomodoro:finish-rest-hook
+;;           (lambda ()
+;;             (my/pomodoro-notification :body "Break time is finished")))
 
-;; 長期休憩後の hook
-(add-hook 'pomodoro:long-rest-hook
-          (lambda ()
-            (my/pomodoro-notification :body "Long Break time now")))
+;; ;; 長期休憩後の hook
+;; (add-hook 'pomodoro:long-rest-hook
+;;           (lambda ()
+;;             (my/pomodoro-notification :body "Long Break time now")))
