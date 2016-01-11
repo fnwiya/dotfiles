b6fa7848110d38a6c96a9fba7ed7a4ce73a42808
fnwiya <fnwiya@gmail.com>
2016-01-11 15:37:08 +0900

    update

--- setup/.emacs.d/loader-init/75-meigen.el
+++ setup/.emacs.d/loader-init/75-meigen.el
@@ -1,4 +1,7 @@
 (add-to-list 'load-path "~/.emacs.d/elisp/work/meigen-bot")
 (use-package meigen-bot
   :config
-  (setq meigen-file-path "~/.emacs.d/elisp/work/meigen-bot/meigen.txt"))
+  (setq meigen-file-path "~/.emacs.d/elisp/work/meigen-bot/meigen.txt")
+  (add-hook 'after-init-hook
+          (lambda ()
+            (echo-meigen-to-minibuffer))))
