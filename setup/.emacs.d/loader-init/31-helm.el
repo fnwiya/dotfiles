0c0def92ce4b3cb5efb5fd5bd9521ad4552dd8d3
fnwiya <fnwiya@gmail.com>
2016-01-14 14:01:07 +0900

    update

--- setup/.emacs.d/loader-init/31-helm.el
+++ setup/.emacs.d/loader-init/31-helm.el
@@ -1,7 +1,6 @@
 (use-package helm-config
   :config
   (helm-mode 1)
-  ;; (helm-migemo-mode 1)
   (setq helm-buffers-fuzzy-matching t)
   (custom-set-variables '(helm-ff-auto-update-initial-value nil))
   (define-key global-map (kbd "M-x")     'helm-M-x)
