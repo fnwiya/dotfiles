9d07bd6a7c5e1e622d828b06fb75e0e905aa5acf
fnwiya <fnwiya@gmail.com>
2016-01-19 18:59:05 +0900

    update

--- install/apt-get.sh
+++ install/apt-get.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 # develop-tool
 sudo apt-get install ansible
 sudo apt-get install build-essential
