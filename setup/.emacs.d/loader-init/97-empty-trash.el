48a103170d5e1d09328f2a0b9d8cc0488b0f6b9f
fnwiya <fnwiya@gmail.com>
2016-01-21 21:45:29 +0900

    update

--- setup/.emacs.d/loader-init/97-empty-trash.el
+++ setup/.emacs.d/loader-init/97-empty-trash.el
@@ -2,3 +2,8 @@
    (add-hook 'kill-emacs-hook
              (lambda()
                (shell-command "rm -rf ~/.local/share/Trash/*"))))
+(cond
+ ((or (eq window-system 'ns) (eq window-system 'mac))
+   (add-hook 'kill-emacs-hook
+             (lambda()
+               (shell-command "rm -rf ~/.Trash/*")))))
