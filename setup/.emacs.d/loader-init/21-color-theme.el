053910301ae6b823455860286425b9cb600bc589
fnwiya <fnwiya@gmail.com>
2016-02-01 02:15:28 +0900

    update

--- setup/.emacs.d/loader-init/21-color-theme.el
+++ setup/.emacs.d/loader-init/21-color-theme.el
@@ -1,8 +1,17 @@
 (when window-system
-   (setq custom-theme-directory "~/.emacs.d/themes/")
-   ;; (load-theme 'fnwiya t)
-   ;; (enable-theme 'fnwiya)
-   (load-theme 'solarized-dark t)
+  (setq custom-theme-directory "~/.emacs.d/themes/")
+  ;; (load-theme 'fnwiya t)
+  ;; (enable-theme 'fnwiya)
+  (setq solarized-distinct-fringe-background t)
+  (setq solarized-use-less-bold t)
+  (setq solarized-use-more-italic t)
+  ;; フォントサイズを変更しない
+  (setq solarized-height-minus-1 1)
+  (setq solarized-height-plus-1 1)
+  (setq solarized-height-plus-2 1)
+  (setq solarized-height-plus-3 1)
+  (setq solarized-height-plus-4 1)
+  (load-theme 'solarized-dark t)
   )
 (unless window-system
-)
+  )
