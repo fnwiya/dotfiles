d99fa055317e86ffb1bf18b159b439faf30459dc
fnwiya <fnwiya@gmail.com>
2016-01-31 22:22:41 +0900

    update

--- setup/profiles.clj
+++ setup/profiles.clj
@@ -1,3 +1,2 @@
-{:repl {:plugins [[cider/cider-nrepl "0.11.0"]
-                  [refactor-nrepl "1.1.0"]]
+{:repl {:plugins [[cider/cider-nrepl "0.10.2"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
