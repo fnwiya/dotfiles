1c20d142e9d6e0d551dd488d74e6dce80e34456c
fnwiya <fnwiya@gmail.com>
2016-02-01 13:58:28 +0900

    update

--- setup/.emacs.d/loader-init/98-after-init.el
+++ setup/.emacs.d/loader-init/98-after-init.el
@@ -12,7 +12,7 @@
               (set-frame-position (selected-frame) 0 0)
               (set-frame-size (selected-frame) 101 50)
               ;; (save-window-excursion
-              ;;   (async-shell-command "sudo apt-get update && sudo apt-get upgrade && sudo apt-get autoremove"))
+                 (async-shell-command "sudo apt-get update && sudo apt-get upgrade && sudo apt-get autoremove");;)
               )))
 
 (when (window-system)
