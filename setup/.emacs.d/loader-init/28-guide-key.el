7475c9a6f8e6dde4cdc358ddca221ddc15f1c2ca
fnwiya <fnwiya@gmail.com>
2016-02-09 23:45:39 +0900

    update

--- setup/.emacs.d/loader-init/28-guide-key.el
+++ setup/.emacs.d/loader-init/28-guide-key.el
@@ -1,5 +1,12 @@
 (use-package guide-key
   :config
   (setq guide-key/popup-window-position 'bottom)
-  (setq guide-key/guide-key-sequence '("C-x r" "C-c" "SPC"))
+  (setq guide-key/guide-key-sequence '("C-x r" "C-c" "SPC" (org-mode "C-c C-x")))
+  (setq guide-key/idle-delay 0.5)
+  (setq guide-key/recursive-key-sequence-flag t)
+  (setq guide-key/text-scale-amount -0.5)
+  (setq guide-key/highlight-command-regexp
+      '("rectangle"
+        ("register" . font-lock-type-face)
+        ("bookmark" . "hot pink")))
   (guide-key-mode 1))
