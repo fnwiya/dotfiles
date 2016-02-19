be5d95f52f520881d2a49bd1d2ffe4f36420a524
fnwiya <fnwiya@gmail.com>
2016-02-19 16:21:45 +0900

    update

--- setup/.emacs.d/loader-init/70-python-mode.el
+++ setup/.emacs.d/loader-init/70-python-mode.el
@@ -4,4 +4,7 @@
   :config
   (setq indent-level 4)
   (setq tab-width 4)
+  (if (equal system-name-simple "forcia-VirtualBox")
+      (setq-default indent-tabs-mode t)
+      (setq-default indent-tabs-mode nil))
   )
