e0fa3aa4cf7df5c7261c75f66dfeeb4553a3ae9b
fnwiya <fnwiya@gmail.com>
2016-02-22 16:08:15 +0900

    update

--- setup/.emacs.d/loader-init/84-evil-surround.el
+++ setup/.emacs.d/loader-init/84-evil-surround.el
@@ -1,6 +1,6 @@
 ;; https://github.com/timcharper/evil-surround
 (use-package evil-surround
-  ;; word:ysiw<somekey> / line:yss<somekey>
+  ;; word: ysiw<somekey> / line: yss<somekey>
   :config
   (global-evil-surround-mode 1)
   (push '(?< . ("< " . " >")) evil-surround-pairs-alist))
