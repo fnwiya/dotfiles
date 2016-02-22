838285846c9bd5113ab9ac6bf4a0838764811574
fnwiya <fnwiya@gmail.com>
2016-02-22 11:23:59 +0900

    update

--- setup/.emacs.d/loader-init/89-evil-keybind.el
+++ setup/.emacs.d/loader-init/89-evil-keybind.el
@@ -47,7 +47,7 @@
 (define-key evil-insert-state-map "\C-t" 'other-window)
 (define-key evil-visual-state-map "\C-t" 'other-window)
 (define-key evil-motion-state-map "gt" 'tabbar-forward-tab)     ; タブ
-(define-key evil-motion-state-map "gr" 'tabbar-backward-tab)    ; タブ
+(define-key evil-motion-state-map "gT" 'tabbar-backward-tab)    ; タブ
 (define-key evil-normal-state-map (kbd "j")  'evil-next-visual-line)     ; 物理行移動
 (define-key evil-normal-state-map (kbd "k")  'evil-previous-visual-line) ; 物理行移動
 (define-key evil-normal-state-map (kbd "gj") 'evil-next-line)
