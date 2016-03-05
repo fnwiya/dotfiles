01168e248d5e8308178d37210e3d96e5161242c4
fnwiya <fnwiya@gmail.com>
2016-03-05 23:02:50 +0900

    update

--- setup/.emacs.d/loader-init/70-clojure-mode.el
+++ setup/.emacs.d/loader-init/70-clojure-mode.el
@@ -4,8 +4,19 @@
   )
 
 (use-package cider
-  :commands (cider-mode)
   :init
-  (add-hook 'clojure-mode-hook 'cider-mode)
+  (add-hook 'cider-mode-hook #'clj-refactor-mode)
+  :config
+  (setq nrepl-log-messages t
+        cider-repl-display-in-current-window t
+        cider-repl-use-clojure-font-lock t
+        cider-prompt-save-file-on-load 'always-save
+        cider-font-lock-dynamically '(macro core function var)
+        cider-overlays-use-font-lock t)
+  (cider-repl-toggle-pretty-printing)
   )
-(use-package clj-refactor)
+(use-package clj-refactor
+  :diminish clj-refactor-mode
+  :config
+  (cljr-add-keybindings-with-prefix "C-c j")
+  )
--- setup/profiles.clj
+++ setup/profiles.clj
@@ -1,2 +1,5 @@
-{:repl {:plugins [[cider/cider-nrepl "0.10.2"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
+{:repl {:plugins [[cider/cider-nrepl "0.10.2"]
+                  [refactor-nrepl "2.0.0-SNAPSHOT"]
+                  [lein-try "0.4.3"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
+                       [alembic "0.3.2"]]}}
