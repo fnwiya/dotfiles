5f508705b6034d6276f2118800bf5679a4952c42
fnwiya <fnwiya@gmail.com>
2016-03-08 09:02:25 +0900

    update

--- install/npm.sh
+++ install/npm.sh
@@ -4,3 +4,8 @@ sudo npm install -g express-generator
 sudo npm install -g tern
 sudo npm install -g mocha
 sudo npm install -g bower
+sudo npm install -g webpack
+sudo npm install -g browserify
+sudo npm install -g babel-cli
+sudo npm install -g eslint
+sudo npm install -g cross-env
