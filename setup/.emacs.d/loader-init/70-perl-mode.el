07409c8d0562873da9aebe36e822b33c7c3e91ed
fnwiya <fnwiya@gmail.com>
2016-03-08 13:38:36 +0900

    update

--- setup/.emacs.d/loader-init/70-perl-mode.el
+++ setup/.emacs.d/loader-init/70-perl-mode.el
@@ -7,12 +7,11 @@
    ("miniperl" . cperl-mode))
   :config
   (setq cperl-indent-level 4
-   cperl-continued-statement-offset 4
-   cperl-close-paren-offset -4
-   cperl-label-offset -4
-   cperl-comment-column 40
-   cperl-highlight-variables-indiscriminately t
-   cperl-indent-parens-as-block t
-   cperl-tab-always-indent nil
-   cperl-font-lock t)
-)
+        cperl-continued-statement-offset 4
+        cperl-close-paren-offset -4
+        cperl-label-offset -4
+        cperl-comment-column 40
+        cperl-highlight-variables-indiscriminately t
+        cperl-indent-parens-as-block t
+        cperl-tab-always-indent nil
+        cperl-font-lock t))
