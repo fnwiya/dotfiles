773afa82593f5348b032b65cbf911d039038a3e1
fnwiya <fnwiya@gmail.com>
2016-03-23 20:22:25 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
+ln -sf ~/dotfiles/setup/bash/.bashrc ~
+ln -sf ~/dotfiles/setup/bash/.bash_profile ~
+ln -sf ~/dotfiles/setup/.conkyrc ~
+ln -sf ~/dotfiles/setup/.emacs.d ~
 ln -sf ~/dotfiles/setup/.gitconfig ~
 ln -sf ~/dotfiles/setup/.gitignore_global ~
-ln -sf ~/dotfiles/setup/.hgrc ~
 ln -sf ~/dotfiles/setup/.hgignore_global ~
-ln -sf ~/dotfiles/setup/.tmux.conf ~
+ln -sf ~/dotfiles/setup/.hgrc ~
+mkdir -p ~/.lein
+ln -sf ~/dotfiles/setup/lein/profiles.clj ~/.lein/profiles.clj
 ln -sf ~/dotfiles/setup/.peco ~
 ln -sf ~/dotfiles/setup/.psqlrc ~
 ln -sf ~/dotfiles/setup/.sbclrc ~
 ln -sf ~/dotfiles/setup/.tern-config ~
-ln -sf ~/dotfiles/setup/.conkyrc ~
-mkdir -p ~/.lein
-ln -sf ~/dotfiles/setup/profiles.clj ~/.lein/profiles.clj
+ln -sf ~/dotfiles/setup/.tmux.conf ~
 ln -sf ~/dotfiles/setup/vim/.vimrc ~
 ln -sf ~/dotfiles/setup/vim/.vim ~
-ln -sf ~/dotfiles/setup/.emacs.d ~
 ln -sf ~/dotfiles/setup/zsh/.zshrc ~
 ln -sf ~/dotfiles/setup/zsh/.zshenv ~
 ln -sf ~/dotfiles/setup/zsh/.zsh.d ~
-ln -sf ~/dotfiles/setup/bash/.bashrc ~
-ln -sf ~/dotfiles/setup/bash/.bash_profile ~
 if [ -e $HOME/Dropbox/config-file ]; then
     ln -sf ~/Dropbox/config-file/.netrc ~
 fi
