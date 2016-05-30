05981927626c715827e32e611885a6a4adcef9ec
fnwiya <fnwiya@gmail.com>
2016-05-30 09:40:48 +0900

    set autojump

--- setup/zsh/.zsh.d/autojump.zsh
+++ setup/zsh/.zsh.d/autojump.zsh
@@ -1,4 +1,12 @@
-[[ -s /usr/share/autojump/autojump.zsh ]] && . /usr/share/autojump/autojump.zsh
 
-# for homebrew
-[[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
+# OS 別の設定
+case ${OSTYPE} in
+    darwin*)
+        #Mac用の設定
+        [[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
+         ;;
+    linux*)
+        #Linux用の設定
+       [[ -s /usr/share/autojump/autojump.zsh ]] && . /usr/share/autojump/autojump.zsh
+       ;;
+esac
