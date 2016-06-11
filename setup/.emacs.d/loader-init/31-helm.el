b1f6fff2f2b2c80dae468d622e7520b9354071d9
fnwiya <fnwiya@gmail.com>
2016-06-11 19:09:14 +0900

    update

--- setup/.emacs.d/loader-init/31-helm.el
+++ setup/.emacs.d/loader-init/31-helm.el
@@ -72,5 +72,4 @@
   :config
   (eval-after-load 'makefile-mode
     '(define-key makefile-mode-map (kbd "M-\"") 'helm-make-projectile))
-  (define-key c-mode-map (kbd "M-\"") 'helm-make-projectile)
   (define-key c++-mode-map (kbd "M-\"") 'helm-make-projectile))
