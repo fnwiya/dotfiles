03ab75d1288d26361923dce4baa73c0a6089a471
fnwiya <fnwiya@gmail.com>
2016-07-02 00:23:08 +0900

    update

--- install/go.sh
+++ install/go.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-go get github.com/motemen/ghq
-go get github.com/peco/peco/cmd/peco
-go get github.com/variadico/noti/cmd/noti
+go get -u github.com/motemen/ghq
+go get -u github.com/peco/peco/cmd/peco
+go get -u github.com/variadico/noti/cmd/noti
+go get -u github.com/kurehajime/pera
