f2aff6112d2ccb7d5eb20a09ebc7825bf2bab876
fnwiya <fnwiya@gmail.com>
2016-07-03 13:28:27 +0900

    update

--- install/npm.sh
+++ install/npm.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+sudo npm install -g babel-cli
+sudo npm install -g bower
+sudo npm install -g browserify
+sudo npm install -g chai
+sudo npm install -g cross-env
+sudo npm install -g eslint
 sudo npm install -g express
 sudo npm install -g express-generator
-sudo npm install -g tern
+sudo npm install -g jasmine
 sudo npm install -g mocha
-sudo npm install -g bower
+sudo npm install -g tern
 sudo npm install -g webpack
-sudo npm install -g browserify
-sudo npm install -g babel-cli
-sudo npm install -g eslint
-sudo npm install -g cross-env
