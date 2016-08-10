a411daea4bcdb1a2cb7efcf664d11dd7b0b27c60
fnwiya <fumiya_kohno@forcia.com>
2016-08-10 09:43:40 +0900

    update

---
+++ setup/.emacs.d/loader-init/70-moz.el
@@ -0,0 +1,24 @@
+(autoload 'moz-minor-mode "moz" "Mozilla Minor and Inferior Mozilla Modes" t)
+
+(add-hook 'js2-mode-hook 'js2-moz-setup)
+(defun js2-moz-setup ()
+  (moz-minor-mode 1))
+
+(add-hook 'web-mode-hook 'web-moz-setup)
+(defun web-moz-setup ()
+  (moz-minor-mode 1))
+
+(add-hook 'css-mode-hook 'css-moz-setup)
+(defun css-moz-setup ()
+  (moz-minor-mode 1))
+
+(defun moz-send-reload ()
+  (interactive)
+  (comint-send-string (inferior-moz-process)
+                      (concat moz-repl-name ".pushenv('printPrompt', 'inputMode'); "
+                              moz-repl-name ".setenv('inputMode', 'line'); "
+                              moz-repl-name ".setenv('printPrompt', false); undefined; "))
+  (comint-send-string (inferior-moz-process)
+                      (concat "content.location.reload();\n"
+                              moz-repl-name ".popenv('inputMode', 'printPrompt'); undefined;\n"))
+  )
