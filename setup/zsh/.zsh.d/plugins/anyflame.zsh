7b0665e9279c81c695a8b0dc9010fd7206995fe2
fnwiya <fnwiya@gmail.com>
2016-08-17 01:06:37 +0900

    update

--- setup/zsh/.zsh.d/plugins/anyflame.zsh
+++ setup/zsh/.zsh.d/plugins/anyflame.zsh
@@ -1,4 +1,9 @@
 if zplug check "mollifier/anyframe"; then
+    autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+    add-zsh-hook chpwd chpwd_recent_dirs
+    # expressly specify to use peco
+    zstyle ":anyframe:selector:" use peco
+    zstyle ":anyframe:selector:peco:" command 'peco'
     bindkey '^xb' anyframe-widget-cdr
     bindkey '^x^b' anyframe-widget-checkout-git-branch
 
--- setup/zsh/.zsh.d/zplug.zsh
+++ setup/zsh/.zsh.d/zplug.zsh
@@ -1,10 +1,12 @@
 # http://blog.b4b4r07.com/entry/2015/12/13/174209
+# https://github.com/zplug/zplug
 if [ -x "`which zplug`" ] ;then
     curl -sL zplug.sh/installer | zsh
 fi
 source ~/.zplug/init.zsh
 
 # プラグイン定義
+zplug "zplug/zplug"
 zplug "zsh-users/zsh-history-substring-search"
 zplug 'zsh-users/zsh-syntax-highlighting'
 zplug "mollifier/anyframe"
