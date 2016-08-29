69501a8c86483c21b962b8bfb88311f0e511f7e6
fumiya_kohno <fumiya_kohno@forcia.com>
2016-08-29 15:10:48 +0900

    update

--- setup/.xmonad/xmobar.hs
+++ setup/.xmonad/xmobar.hs
@@ -19,9 +19,21 @@ Config {
                     , Run Swap [
                         ] 10
                     , Run Date "%a %b %_d %Y %H:%M:%S" "date" 10
+                    , Run BatteryP ["CMB1"]
+                                   [
+                        "-t", " <acstatus> "
+                        ,"-L", "20"
+                        ,"-H","80"
+                        ,"--low","#f44336"
+                        ,"--normal","#ffffff"
+                        ,"--",
+                          "-o","<left>%(<timeleft>)"
+                          ,"-O","Charging<left>%"
+                          ,"-i","<left>%"
+                        ] 50
                     , Run StdinReader
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = "%cpu% | %memory% | %swap% } %StdinReader% { %date% "
+       , template = "%cpu% | %memory% | %swap% } %StdinReader% { %battery%%date% "
        }
