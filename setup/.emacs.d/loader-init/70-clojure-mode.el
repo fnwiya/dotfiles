6990c6dc749caa09019aa4c729bba547dda006ef
fumiya_kohno <fumiya_kohno@forcia.com>
2016-09-29 00:57:31 +0900

    update

--- setup/.emacs.d/loader-init/70-clojure-mode.el
+++ setup/.emacs.d/loader-init/70-clojure-mode.el
@@ -6,14 +6,13 @@
   :init
   (add-hook 'cider-mode-hook #'clj-refactor-mode)
   :config
-  ;; (setq nrepl-log-messages t
-  ;;       cider-repl-display-in-current-window t
-  ;;       cider-repl-use-clojure-font-lock t
-  ;;       cider-prompt-save-file-on-load 'always-save
-  ;;       cider-font-lock-dynamically '(macro core function var)
-  ;;       cider-overlays-use-font-lock t)
-  ;; (cider-repl-toggle-pretty-printing)
-  )
+  (setq nrepl-log-messages t
+        cider-repl-display-in-current-window t
+        cider-repl-use-clojure-font-lock t
+        cider-prompt-save-file-on-load 'always-save
+        cider-font-lock-dynamically '(macro core function var)
+        cider-overlays-use-font-lock t)
+  (cider-repl-toggle-pretty-printing))
 
 (use-package clj-refactor
   :diminish clj-refactor-mode
