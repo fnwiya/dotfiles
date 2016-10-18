2dcf55efae39db5e909cdf9b64e4e01822231c92
fumiya_kohno <fumiya_kohno@forcia.com>
2016-10-18 11:10:30 +0900

    update

--- setup/.emacs.d/loader-init/70-org-mode.el
+++ setup/.emacs.d/loader-init/70-org-mode.el
@@ -1,3 +1,5 @@
 (use-package org)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (add-to-list 'auto-mode-alist '("\\.org_archive$" . org-mode))
+(setq org-default-notes-file "~/Dropbox/backup/memo.org")
+(setq org-agenda-files (list org-default-notes-file ))
