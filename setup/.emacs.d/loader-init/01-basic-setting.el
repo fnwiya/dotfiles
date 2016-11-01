715d07a3805230993faa7cbd8ca4476c63702ad5
fumiya_kohno <fumiya_kohno@forcia.com>
2016-11-01 21:22:10 +0900

    update

--- setup/.emacs.d/loader-init/01-basic-setting.el
+++ setup/.emacs.d/loader-init/01-basic-setting.el
@@ -69,3 +69,15 @@
 
 ;; Macのoptionをメタキーにする
 (setq mac-option-modifier 'meta)
+
+;; クリップボードと同期
+(setq interprogram-paste-function
+      (lambda ()
+        (shell-command-to-string "xsel -b -o")))
+(setq interprogram-cut-function
+      (lambda (text && optional rest)
+        (let *  (process-connection-type nil)
+             (proc (start-process "xsel" "*Messages*" "xsel" "-b" "-i")))
+        (process-send-string proc text)
+        (process-send-eof proc)))
+
