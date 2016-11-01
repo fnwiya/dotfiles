555330fbfa59a187ed0da32de77a06df5667b86f
fumiya_kohno <fumiya_kohno@forcia.com>
2016-11-01 21:31:22 +0900

    update

--- setup/.emacs.d/loader-init/01-basic-setting.el
+++ setup/.emacs.d/loader-init/01-basic-setting.el
@@ -73,10 +73,10 @@
 ;; クリップボードと同期
 (setq interprogram-paste-function
       (lambda ()
-        (shell-command-to-string "xsel -b -o")))
+         (shell-command-to-string "xsel -b -o")))
 (setq interprogram-cut-function
-      (lambda (text && optional rest)
-        (let*  (process-connection-type nil)
-             (proc (start-process "xsel" "*Messages*" "xsel" "-b" "-i")))
-        (process-send-string proc text)
-        (process-send-eof proc)))
+      (lambda (text &optional rest)
+         (let* ((process-connection-type nil)
+                (proc (start-process "xsel" "*Messages*" "xsel" "-b" "-i")))
+           (process-send-string proc text)
+           (process-send-eof proc))))
