701a6ae5f16a09bd57425bfadbef3fc9ae480e75
fumiya_kohno <fumiya_kohno@forcia.com>
2016-11-05 14:23:21 +0900

    update

--- setup/.emacs.d/loader-init/88-evil-key-combo.el
+++ setup/.emacs.d/loader-init/88-evil-key-combo.el
@@ -44,10 +44,11 @@
               ))
   (add-hook 'haskell-mode-hook
             (lambda()
-              (key-combo-define evil-insert-state-map (kbd "<")  '("<" "<-"))
-              (key-combo-define evil-insert-state-map (kbd ">")  '(">" "->" ">>="))
+              (key-combo-define evil-insert-state-map (kbd "<")  '("<" "<-" " <= "))
+              (key-combo-define evil-insert-state-map (kbd ">")  '(">" "->" " => "  ">>="))
               (key-combo-define evil-insert-state-map (kbd "$")  '(" $ " "$>" " <$> "))
-              (key-combo-define evil-insert-state-map (kbd "|") '(" | " "|"))
+              (key-combo-define evil-insert-state-map (kbd "|") '(" | " " || " "|"))
+              (key-combo-define evil-insert-state-map (kbd "/") '(" / " " /= " "/"))
               ))
   (add-hook 'clojure-mode-hook
             (lambda()
