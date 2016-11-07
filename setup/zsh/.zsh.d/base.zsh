0707f01ebf07223c17e9e7721c0b7f66e5d507b3
fumiya_kohno <fumiya_kohno@forcia.com>
2016-11-07 09:57:40 +0900

    update

--- setup/zsh/.zsh.d/base.zsh
+++ setup/zsh/.zsh.d/base.zsh
@@ -5,6 +5,7 @@ export LANG=ja_JP.UTF-8
 export KCODE=u
 export EDITOR=vim
 export SHELL=zsh
+export LESS='-g -i -M -R -W -z-4'
 ## vimがなくてもvimでviを起動する。
 if ! type vim > /dev/null 2>&1; then
     alias vim=vi
