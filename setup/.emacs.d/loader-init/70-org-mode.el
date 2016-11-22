badbf90fffa080382948b6d193c1069178aeb4f5
fumiya_kohno <fumiya_kohno@forcia.com>
2016-11-22 17:49:23 +0900

    update

--- setup/.emacs.d/loader-init/70-org-mode.el
+++ setup/.emacs.d/loader-init/70-org-mode.el
@@ -1,7 +1,7 @@
 (use-package org)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (add-to-list 'auto-mode-alist '("\\.org_archive$" . org-mode))
-(setq org-default-notes-file "~/Dropbox/backup/memo.org")
+;; (setq org-default-notes-file "~/Dropbox/backup/memo.org")
 (setq org-agenda-files (list org-default-notes-file ))
 (setq org-todo-keywords
       '((sequence "TODO(t)" "WAIT(w)" "|" "DONE(d)" "SOMEDAY(s)")))
