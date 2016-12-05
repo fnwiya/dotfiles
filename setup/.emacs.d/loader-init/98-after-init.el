9d9b52ebadcdea5454cbc0bb7473165a08df499e
fumiya_kohno <fumiya_kohno@forcia.com>
2016-12-05 14:11:21 +0900

    update

--- setup/.emacs.d/loader-init/98-after-init.el
+++ setup/.emacs.d/loader-init/98-after-init.el
@@ -1,28 +1,25 @@
-(add-hook 'after-init-hook
-          (lambda()
-            (set-frame-position (selected-frame) 0 0)
-            (cond
-             ((or (eq window-system 'ns) (eq window-system 'mac))
-              ;; MacBook Air (11-inch, Mid 2013)
-              (set-frame-size (selected-frame) 91 47)
-              (save-window-excursion
-                (async-shell-command "brew update && brew upgrade && brew doctor")))
-             ((and (window-system) (eq system-type 'gnu/linux))
-              (cond
-               ((equal system-name-simple "forcia-VirtualBox")
-                (set-frame-size (selected-frame) 101 50)
-                (save-window-excursion
-                  (async-shell-command "sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y autoremove"))))))))
-
 (when (window-system)
   (add-hook 'after-init-hook
             (lambda()
+              (set-frame-position (selected-frame) 0 0)
               ;; (setq w (selected-window))
               ;; (setq w2 (split-window w (- (window-height w) 8)))
               ;; (select-window w2)
               ;; (multi-term)
               ;; (select-window w)
-              (recentf-open-files))))
+              (recentf-open-files)
+              (cond
+               ((or (eq window-system 'ns) (eq window-system 'mac))
+                ;; MacBook Air (11-inch, Mid 2013)
+                (set-frame-size (selected-frame) 91 47)
+                (save-window-excursion
+                  (async-shell-command "brew update && brew upgrade && brew doctor")))
+               ((and (window-system) (eq system-type 'gnu/linux))
+                (cond
+                 ((equal system-name-simple "forcia-VirtualBox")
+                  (set-frame-size (selected-frame) 101 50)
+                  (save-window-excursion
+                    (async-shell-command "sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y autoremove")))))))))
 (unless (window-system)
   (add-hook 'after-init-hook
             (lambda()
