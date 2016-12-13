e381d7c745f700c397dc0cf23c4cc052c8e2b954
fumiya_kohno <fumiya_kohno@forcia.com>
2016-12-13 21:54:13 +0900

    edit org-mode todo status

--- setup/.emacs.d/loader-init/70-org-mode.el
+++ setup/.emacs.d/loader-init/70-org-mode.el
@@ -4,5 +4,5 @@
 ;; (setq org-default-notes-file "~/Dropbox/backup/memo.org")
 (setq org-agenda-files (list org-default-notes-file ))
 (setq org-todo-keywords
-      '((sequence "TODO(t)" "WAIT(w)" "|" "DONE(d)" "SOMEDAY(s)")))
+      '((sequence "TODO(t)" "WAIT(w)" "RESCHEDULE(r)" "|" "DONE(d)" "CANCEL(c)")))
 (setq org-log-done 'time)
