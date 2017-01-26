56163f9953050b5ca55f789997d7ddffe79e2161
fumiya_kohno <fumiya_kohno@forcia.com>
2017-01-26 16:15:01 +0900

    update

--- setup/.emacs.d/loader-init/81-evil-leader.el
+++ setup/.emacs.d/loader-init/81-evil-leader.el
@@ -7,21 +7,21 @@
     ":" 'shell-command
     "=" 'indent-region
     "@" 'mark-whole-buffer
-    ;; "a" 'helm-projectile-ag
-    ;; "A" 'helm-ag
-    ;; "b" 'helm-buffers-list
-    ;; "e" 'helm-find-files
-    ;; "r" 'helm-recentf
-    ;; "s" 'helm-swoop
+    "a" 'helm-projectile-ag
+    "A" 'helm-ag
+    "b" 'helm-buffers-list
+    "e" 'helm-find-files
+    "r" 'helm-recentf
+    "s" 'helm-swoop
     ;; "x" 'helm-M-x
-    ;; "y" 'helm-show-kill-ring
-    "a" 'counsel-projectile-ag
-    "A" 'counsel-ag
-    "b" 'ivy-switch-buffer
-    "e" 'counsel-find-file
-    "r" 'ivy-recentf
+    "y" 'helm-show-kill-ring
+    ;; "a" 'counsel-projectile-ag
+    ;; "A" 'counsel-ag
+    ;; "b" 'ivy-switch-buffer
+    ;; "e" 'counsel-find-file
+    ;; "r" 'ivy-recentf
     "x" 'counsel-M-x
-    "y" 'counsel-yank-pop
+    ;; "y" 'counsel-yank-pop
     "f" 'counsel-projectile-find-file
     "d" 'counsel-projectile-find-dir
     "g" 'avy-goto-word-0
