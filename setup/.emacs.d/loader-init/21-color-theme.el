797356ceab36ff3dc0a23e5264d8ccd71dc9303e
fumiya_kohno <fumiya_kohno@forcia.com>
2017-02-17 18:56:34 +0900

    update

--- setup/.emacs.d/loader-init/21-color-theme.el
+++ setup/.emacs.d/loader-init/21-color-theme.el
@@ -3,8 +3,6 @@
   (load-theme 'fnwiya t)
   (enable-theme 'fnwiya))
 (unless (window-system)
-  (use-package solarized
-    :config
-    (set-frame-parameter nil 'background-mode 'dark)
-    (set-terminal-parameter nil 'background-mode 'dark)
-    (enable-theme 'solarized)))
+    (color-theme-initialize)
+    (load-theme 'solarized-dark t)
+    (enable-theme 'solarized-dark))
