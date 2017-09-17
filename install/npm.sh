ce6c236a6913f0d9f32b403b2e458012d45dcdca
fnwiya <fnwiya@gmail.com>
2017-09-17 19:02:10 +0900

    update

--- install/npm.sh
+++ install/npm.sh
@@ -10,6 +10,7 @@ sudo npm install -g express
 sudo npm install -g express-generator
 sudo npm install -g jasmine
 sudo npm install -g karma
+sudo npm install -g mdr
 sudo npm install -g mocha
 sudo npm install -g n
 sudo npm install -g nodemon
