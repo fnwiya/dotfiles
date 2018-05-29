beb267eb8f331e71fb894b1df54631e75b509f5a
fnwiya <fnwiya@gmail.com>
2018-05-29 17:28:25 +0900

    update

--- install/emacs.sh
+++ install/emacs.sh
@@ -1,7 +1,8 @@
+VERSION=26.1
 cd /usr/local/src
-sudo wget http://ftp.gnu.org/pub/gnu/emacs/emacs-25.3.tar.gz
-sudo tar zxvf emacs-25.3.tar.gz
-cd emacs-25.3
-./configure --without-x
+sudo wget http://ftp.gnu.org/pub/gnu/emacs/emacs-${VERSION}.tar.gz
+sudo tar zxvf emacs-${VERSION}.tar.gz
+cd emacs-${VERSION}
+sudo ./configure --without-x
 sudo make
 sudo make install
