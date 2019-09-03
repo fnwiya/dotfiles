db8a878bfb50cc118902703ca8f641c1ebc32862
fnwiya <fnwiya@gmail.com>
2019-09-03 15:24:04 +0900

    Set alias

--- setup/zsh/.zsh.d/alias.zsh
+++ setup/zsh/.zsh.d/alias.zsh
@@ -14,6 +14,7 @@ alias em='emacsclient -n'
 alias ekill='emacsclient -e "(kill-emacs)"'
 alias gitupd='git add --all . && git commit -m "update" && git push'
 alias gitroot='cd $(git rev-parse --show-toplevel)'
+alias gitpullrebase='git stash && git pull --rebase && git stash pop stash@{0}'
 alias multi_ssh='~/dotfiles/setup/tmux/.tmux/multi_ssh.sh'
 alias rust='cargo-script'
 function ssh() {
